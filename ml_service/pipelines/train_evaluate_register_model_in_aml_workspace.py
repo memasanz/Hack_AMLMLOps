f2cc49327b4bc65cc8a6a70c10540642be0aa7af
Megan Masanz <memasanz@microsoft.com>
2021-09-28 13:28:49 -0500

    Updated

--- ml_service/pipelines/train_evaluate_register_model_in_aml_workspace.py
+++ ml_service/pipelines/train_evaluate_register_model_in_aml_workspace.py
@@ -6,7 +6,7 @@ from azureml.core.runconfig import RunConfiguration
 from ml_service.pipelines.load_sample_data import create_sample_data_csv
 from ml_service.util.attach_compute import get_compute
 from ml_service.util.env_variables import Env
-from ml_service.util.manage_environment import 
+from ml_service.util.manage_environment import get_environment
 
 import os
 
